a5aaf3dd4b3d4a78727712e24cc8a6a25a204e14
drilja <ilja@mirsky.eu>
2021-02-21 12:26:08 +0100

    asdf

--- script.js
+++ script.js
@@ -1,29 +1,19 @@
+window.onload = () => {
+    const button = document.querySelector('button[data-action="change"]');
+    button.innerText = '﹖';
 
-
-
-
-
-
-
-  window.onload = () => {
-    const button = document.querySelector('button[data-action="change"]')
-    button.innerText = '?';
-    button.addEventListener("click", function(){
-    console.log("Button wurde geklickt!");
-    // alert("This is marker 1");
-    });    
-
-    var puppet = staticLoadpuppet();
-    changePuppet(puppet);
+    let places = staticLoadPlaces();
+    renderPlaces(places);
 };
 
-function staticLoadpuppet() {
+function staticLoadPlaces() {
     return [
         {
             name: 'Pokèmon',
             location: {
                 // decomment the following and add coordinates:
-          
+                // lat: <your-latitude>,
+                // lng: <your-longitude>,
             },
         },
     ];
@@ -32,22 +22,28 @@ function staticLoadpuppet() {
 var models = [
     {
         url: 'src/bug_v1.glb',
-        scale: '0.1 0.1 0.1',
+        scale: '1 1 1',
         info: 'Bug, Lv. 5, HP 10/10',
         rotation: '0 0 0',
+        class: 'clickable',
+        log: 'entity puppet1'
     },
     {
         url: 'src/frankv1.glb',
-        scale: '0.1 0.1 0.1',
+        scale: '1 1 1',
+        info: 'Bug, Lv. 5, HP 10/10',
         rotation: '0 0 0',
-        info: 'Puppet from Frank, HP 100/100',
+        class: 'clickable',
+        log: 'entity puppet1'
     },
     {
         url: 'src/whale_v1.glb',
-        scale: '0.5 0.5 0.5',
+        scale: '1 1 1',
+        info: 'Bug, Lv. 5, HP 10/10',
         rotation: '0 0 0',
-        info: 'Whale Puppet, Lv. 99, HP 150/150',
-    },
+        class: 'clickable',
+        log: 'entity puppet1'
+    }
 ];
 
 var modelIndex = 0;
@@ -66,42 +62,31 @@ var setModel = function (model, entity) {
 
     entity.setAttribute('gltf-model', model.url);
 
-
-    entity.setAttribute('gesture-handler');
-    entity.setAttribute('class', 'clickable');
-
     const div = document.querySelector('.instructions');
     div.innerText = model.info;
 };
 
-function changePuppet(puppet) {
-    var scene = document.querySelector('a-scene');
+function renderPlaces(places) {
+    let scene = document.querySelector('a-scene');
 
-    puppet.forEach((puppet) => {
-        // var latitude = puppet.location.lat;
-        // var longitude = puppet.location.lng;
+    places.forEach((place) => {
+        let latitude = place.location.lat;
+        let longitude = place.location.lng;
 
-        var model = document.createElement('a-entity');
-        // model.setAttribute('gps-entity-place', 'latitude: ${latitude}; longitude: ${longitude};');
-
-      
-
-        model.setAttribute('animation-mixer', '');
-        model.setAttribute('gesture-handler');
-        model.setAttribute('class', 'clickable');
+        let model = document.createElement('a-entity');
+        model.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
 
         setModel(models[modelIndex], model);
 
-        document.querySelector('button[data-action="change"]').addEventListener('click', function () {
-            var entity = document.querySelector('a-scene');
-            
-            modelIndex++;
+        model.setAttribute('animation-mixer', '');
+        model.setAttribute('gesture-handler');
+        
 
-            // entity.setAttribute('gesture-handler');
-            // entity.setAttribute('class', 'clickable');
+        document.querySelector('button[data-action="change"]').addEventListener('click', function () {
+            var entity = document.querySelector('[gps-entity-place]');
+            modelIndex++;
             var newIndex = modelIndex % models.length;
             setModel(models[newIndex], entity);
-            // setModel(models[newIndex]);
         });
 
         scene.appendChild(model);
--- scriptAframe.js
+++ scriptAframe.js
@@ -5,8 +5,42 @@ AFRAME.registerComponent('log', {
       var stringToLog = this.data;
     //   console.log(stringToLog);
     //   console.log(this.el.sceneEl); 
-    // var sceneEl = document.querySelector('a-scene');
+    var sceneEl = document.querySelector('a-scene');
+    var markerEl = document.querySelector('a-marker');
     // console.log(sceneEl.querySelector('#puppet1'));
     // console.log(document.querySelector('button'))
+    // console.log(sceneEl.querySelectorAll('.clickable'));
+    // var els = sceneEl.querySelectorAll('*');
+    // for (var i = 0; i < els.length; i++) {
+    // console.log(els[i]);
+    // }
+
+    
+  
+
+    document.querySelector('#button').addEventListener('click', function () {
+        console.log("Button was pressed");
+        
+        var entityEl = document.createElement('a-entity');
+// Do `.setAttribute()`s to initialize the entity.
+        
+
+        entityEl.setAttribute('geometry', {
+            primitive: 'box',
+            height: 10,
+            width: 10
+          });
+
+
+        //   sceneEl.appendChild(entityEl);
+          markerEl.appendChild(markerEl);
+          console.log(entityEl);
+
+
+        // puppet1.setAttribute('visible', false);
+
+        // puppet2.object3D.visibile = false;
+        // puppet3.object3D.visibile = false;
+    });
     }
   });
